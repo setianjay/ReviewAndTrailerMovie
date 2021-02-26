f9853904601fd9c9f71c6947d4c49808306ec987
setianjay <hari.setiaji2000@gmail.com>
2021-02-26 11:28:29 +0700

    Membuat project dan splashscreen

---
+++ app/src/main/java/com/setianjay/movieapp/SplashscreenActivity.kt
@@ -0,0 +1,20 @@
+package com.setianjay.movieapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+        supportActionBar!!.hide()
+        val handler = Handler()
+        handler.postDelayed({
+            startActivity(Intent(applicationContext,MainActivity::class.java))
+            finish()
+        },3000)
+    }
+}
