c223b2caa5363b94235954011b408dce38c16a9b
setianjay <hari.setiaji2000@gmail.com>
2021-03-09 18:10:53 +0700

    Menggunakan ViewBinding pada TrailerAdapter

--- app/src/main/java/com/setianjay/movieapp/adapter/TrailerAdapter.kt
+++ app/src/main/java/com/setianjay/movieapp/adapter/TrailerAdapter.kt
@@ -1,43 +1,37 @@
 package com.setianjay.movieapp.adapter
 
-import android.icu.text.CaseMap
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.setianjay.movieapp.R
+import com.setianjay.movieapp.databinding.ItemMovieTrailersBinding
 import com.setianjay.movieapp.model.TrailerModel
 
-class TrailerAdapter(val reviewList: ArrayList<TrailerModel>, val listener: OnAdapterListener) :
+class TrailerAdapter(val trailerList: ArrayList<TrailerModel>, val listener: OnAdapterListener) :
     RecyclerView.Adapter<TrailerAdapter.ViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrailerAdapter.ViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.item_movie_trailers, parent, false)
-        return ViewHolder(view)
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = ViewHolder(
+        ItemMovieTrailersBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+    )
 
-    override fun getItemCount(): Int = reviewList.size
+    override fun getItemCount(): Int = trailerList.size
 
 
     override fun onBindViewHolder(holder: TrailerAdapter.ViewHolder, position: Int) {
-        holder.funBind(reviewList[position])
-        holder.tvLabelTrailer.setOnClickListener{
-            listener.onPlay(reviewList[position].key!!,reviewList[position].name!!)
+        holder.funBind(trailerList[position])
+        holder.binding.tvTitleTrailer.setOnClickListener{
+            listener.onPlay(trailerList[position].key!!,trailerList[position].name!!)
         }
     }
 
 
-    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-        val tvLabelTrailer = view.findViewById<TextView>(R.id.tv_title_trailer)
+    inner class ViewHolder(val binding: ItemMovieTrailersBinding) : RecyclerView.ViewHolder(binding.root) {
         fun funBind(trailer: TrailerModel) {
-            tvLabelTrailer.text = trailer.name
+            binding.tvTitleTrailer.text = trailer.name
         }
     }
 
     fun setData(data: List<TrailerModel>) {
-        reviewList.clear()
-        reviewList.addAll(data)
+        trailerList.clear()
+        trailerList.addAll(data)
         notifyDataSetChanged()
         listener.onLoad(data[0].key!!,data[0].name!!)
     }
